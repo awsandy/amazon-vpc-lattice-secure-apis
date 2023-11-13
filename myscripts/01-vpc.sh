28accc7cb355327cc711318cc3b853e36a0abb4a
awsandy <awsandy@amazon.co.uk>
2023-11-13 16:43:13 +0000

    up

--- myscripts/01-vpc.sh
+++ myscripts/01-vpc.sh
@@ -1,4 +1,4 @@
-cd ../amazon-vpc-lattice-secure-apis/vpc
+cd ~/environment/amazon-vpc-lattice-secure-apis/vpc
 npm install
 cdk synth --context cidr="10.0.0.0/16"
 cdk deploy --context cidr="10.0.0.0/16" --require-approval never
